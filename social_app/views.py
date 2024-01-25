8b59d99c87f1b4b33dbbad3da33ac9574599d944
Taras Ostapchuk <trs.ostp@gmail.com>
2024-01-26 00:00:13 +0100

    Create SignUp - view, template. Create signup logic

--- social_app/views.py
+++ social_app/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from social_app.models import Profile
 
 
 # Create your views here.
@@ -9,3 +13,32 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                #Login user and redirect to setting page
+                #Create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
