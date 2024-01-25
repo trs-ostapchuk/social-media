c7f8cfd2216d641c8647d6199ce390e8d3a2d752
Taras Ostapchuk <trs.ostp@gmail.com>
2024-01-25 16:27:12 +0100

    Create base template

--- social_app/views.py
+++ social_app/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello, Social Media</h1>')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
