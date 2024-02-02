8668a7d6700faf5416111d5b84e7372ebae2139f
Taras Ostapchuk <trs.ostp@gmail.com>
2024-02-02 22:25:32 +0100

    Create likes system

--- social_app/models.py
+++ social_app/models.py
@@ -23,7 +23,15 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
-    no_of_likes = models.ImageField(default=0)
+    no_of_likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
